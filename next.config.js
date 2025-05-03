e06911ab3562580fb6de63d6637c1ff2ea1466e4
fork_me_maybe <forkmemaybe@gmail.com>
2025-05-03 12:40:31 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/forkmemaybe-portfolio',
+};
+
+module.exports = nextConfig;
+
